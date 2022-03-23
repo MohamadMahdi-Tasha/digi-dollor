6c94f366443ccd9232135e977c5212490c37183c
Mohamad Mahdi Tasha <imwhdiiii@gmail.com>
2022-03-23 18:38:37 +0430

    added dark mode of dashboard page

---
+++ assets/scripts/dark_mode.js
@@ -0,0 +1,21 @@
+// Variables
+const switch_toggle = document.getElementById('dv-switch-toggle');
+const dashboard_main = document.getElementById('dv-dashboard-main');
+let clicks_dark_mode = 0;
+
+// A Function That Turns On Dark Theme
+function turn_dark() {
+    if (clicks_dark_mode % 2 !== 0) {
+        dashboard_main.setAttribute('data-theme', 'dark');
+        dashboard_main.classList.add('dark');
+    } else {
+        dashboard_main.setAttribute('data-theme', 'light');
+        dashboard_main.classList.remove('dark');
+    }
+}
+
+// Adding Event Listener On Switch Toggle That Calls 'turn_dark' Function
+switch_toggle.addEventListener('click', () => {
+    clicks_dark_mode ++;
+    turn_dark();
+})
