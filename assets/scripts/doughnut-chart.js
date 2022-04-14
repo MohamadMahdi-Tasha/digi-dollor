42e92abb15452e7a77615393718b015ebcc31692
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-04-14 18:40:46 +0430

    styled first item of middle section

--- assets/scripts/doughnut-chart.js
+++ assets/scripts/doughnut-chart.js
@@ -1,26 +1,30 @@
-const ctx = document.getElementById('doughnut-chart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['orange', 'indigo', 'diamond', 'purple'],
-        datasets: [{
-            label: '# of Votes',
-            data: [25, 25, 10, 40],
-            backgroundColor: [
-                '#FCA120',
-                '#185ADB',
-                '#5FC8A5',
-                'purple'
-            ],
-        }]
+function DrawTheChart(ChartData, ChartOptions, ChartId, ChartType) {eval('var myLine = new Chart(document.getElementById(ChartId).getContext("2d"),{type:"' + ChartType + '",data:ChartData,options:ChartOptions});document.getElementById(ChartId).getContext("2d").stroke();')}
+const ChartData = {
+    labels: ["X", "Y", "Z", "M"],
+    datasets: [
+        {
+            data: [50, 25, 50, 10],
+            backgroundColor: ['#5FC8A5', '#185ADB', 'white', '#FCA120'],
+        },
+    ]
+};
+
+ChartOptions = {
+    responsive: true,
+    layout: {padding: {top: 12, left: 12, bottom: 12,},},
+    plugins: {legend: {display: false,}},
+    title: {
+        display: true,
+        text: 'Chart Title',
+        fontColor: '#3498db',
+        fontSize: 32,
+        fontStyle: ' bold',
     },
-    options: {
-        plugins: {legend: {display: false,}},
-        responsive: true,
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+    elements: {
+        arc: {
+            borderWidth: 0.01,
+        },
+    },
+};
+
+DrawTheChart(ChartData, ChartOptions, "doughnut-chart", "doughnut");
