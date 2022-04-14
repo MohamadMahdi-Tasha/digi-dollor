7919cba36cfe578fe533d6a67f27f6cb4eeb4d8e
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-04-14 20:23:22 +0430

    added styles of second item of middle section of dashboard page

--- assets/scripts/line_chart.js
+++ assets/scripts/line_chart.js
@@ -1,17 +1,32 @@
-const ctx_2 = document.getElementById('line-chart').getContext('2d');
-const line_chart = new Chart(ctx_2, {
+const ctx = document.getElementById('line-chart').getContext('2d');
+const myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['1', '2', '3', '4', '5', '6'],
+        labels: ['دی ۱۳۹۹', 'بمهن ۱۳۹۹', 'بمهن ۱۳۹۹', 'فروردین  ۱۴۰۰', 'اردیبهشت  ۱۴۰۰'],
         datasets: [{
-            label: '# of Votes',
-            data: [0, 20, 10, 15, 25, 30],
+            data: [0, 15, 30, 15, 92],
+            backgroundColor: ['white',],
+            borderColor: ['#0A1931',],
+            borderWidth: 1
         }]
     },
     options: {
         responsive: true,
         plugins: {
             legend: {display: false,},
-        }
+            title: {display: false,},
+        },
+        scales: {
+            x: {
+                grid: {
+                    display: false
+                }
+            },
+            y: {
+                grid: {
+                    display: false
+                }
+            }
+        },
     },
 });
