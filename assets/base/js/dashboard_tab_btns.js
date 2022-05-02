11c6d8e414d13aa871c7b1d64290b200fff78aa6
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-05-02 11:09:04 +0430

    added js of tables tab btns

--- assets/base/js/dashboard_tab_btns.js
+++ assets/base/js/dashboard_tab_btns.js
@@ -1,5 +1,19 @@
-document.querySelectorAll('.dv-dashboard-tab-btn').forEach(item => {
-    item.addEventListener('click', () => {
-        console.log(item.textContent)
-    })
-})
+// Variables
+const tab_btn_2_1 = document.getElementById('dv-tab-btn-2-1');
+const tab_btn_2_2 = document.getElementById('dv-tab-btn-2-2');
+const tab_btn_2_3 = document.getElementById('dv-tab-btn-2-3');
+const tab_btn_2_4 = document.getElementById('dv-tab-btn-2-4');
+
+// A Function That Activates Given Button And DeActivates Others
+function activate_tab_btn(btn, btn2, btn3, btn4) {
+    btn.classList.add('active');
+    btn2.classList.remove('active');
+    btn3.classList.remove('active');
+    btn4.classList.remove('active');
+}
+
+// Adding Event Listener On Each Tab Button That Listens To Click And Calls 'activate_tab_btn' Function
+tab_btn_2_1.addEventListener('click', () => activate_tab_btn(tab_btn_2_1, tab_btn_2_2, tab_btn_2_3, tab_btn_2_4));
+tab_btn_2_2.addEventListener('click', () => activate_tab_btn(tab_btn_2_2, tab_btn_2_1, tab_btn_2_3, tab_btn_2_4));
+tab_btn_2_3.addEventListener('click', () => activate_tab_btn(tab_btn_2_3, tab_btn_2_1, tab_btn_2_2, tab_btn_2_4));
+tab_btn_2_4.addEventListener('click', () => activate_tab_btn(tab_btn_2_4, tab_btn_2_1, tab_btn_2_2, tab_btn_2_3));
