a704b728378e77bb7c5f98d08edd7c72d50ae9d6
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-05-02 12:01:53 +0430

    added media qurys of dashboard page

--- assets/base/js/dark_mode.js
+++ assets/base/js/dark_mode.js
@@ -1,16 +1,27 @@
 // Variables
 const switch_toggle = document.getElementById('dv-switch-toggle');
 const dashboard_main = document.getElementById('dv-dashboard-main');
+const header_menu_theme_toggler = document.getElementById('dv-header-menu-theme-toggler');
 let clicks_dark_mode = 0;
+let clicks_dark_mode_header_menu = 0;
 
 // A Function That Turns On Dark Theme
-function turn_dark() {
-    if (clicks_dark_mode % 2 !== 0) {dashboard_main.setAttribute('data-theme', 'dark');}
+function turn_dark(clicks) {
+    if (clicks % 2 !== 0) {dashboard_main.setAttribute('data-theme', 'dark');}
     else {dashboard_main.setAttribute('data-theme', 'light');}
 }
 
 // Adding Event Listener On Switch Toggle That Calls 'turn_dark' Function
 switch_toggle.addEventListener('click', () => {
     clicks_dark_mode ++;
-    turn_dark();
+    turn_dark(clicks_dark_mode);
+})
+
+// Adding Event Listener On Header Profile Dropdown, Theme Toggler Button That Listens To Click And  Calls 'turn_dark' Function
+header_menu_theme_toggler.addEventListener('click', () => {
+    clicks_dark_mode_header_menu ++;
+    const theme_status = document.getElementById('dv-dashboard-main').getAttribute('data-theme');
+    if (theme_status === 'dark') {header_menu_theme_toggler.lastElementChild.textContent = 'روشن';}
+    else {header_menu_theme_toggler.lastElementChild.textContent = 'تیره';}
+    turn_dark(clicks_dark_mode_header_menu);
 })
