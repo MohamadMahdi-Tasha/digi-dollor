a6504413bc0dff723bb9964be463f95017a62111
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-05-03 15:54:42 +0430

    deleted unusable files, edited upload-media.js and linked sweetalert

--- assets/base/js/upload_media.js
+++ assets/base/js/upload_media.js
@@ -1,25 +1,25 @@
 // Variables
 const upload_media_btn = document.getElementById('dv-upload-media-btn');
+let clicks_of_upload_btn = 0;
 
 // A Function That Gives The User Ability To Upload Media.
 let file = [];
 function upload_media() {
-    // Creating An Input
-    const upload_input = document.createElement('input');
+    clicks_of_upload_btn ++;
+    if (clicks_of_upload_btn <= 3) {
+        // Creating An Input
+        const upload_input = document.createElement('input');
 
-    // Setting Some Attributes To Input Created
-    upload_input.id = 'dv-upload-input';
-    upload_input.type = 'file';
-    upload_input.accept = 'image/png, image/jpeg, image/jpg';
+        // Setting Some Attributes To Input Created
+        upload_input.id = 'dv-upload-input';
+        upload_input.type = 'file';
+        upload_input.accept = 'image/png, image/jpeg, image/jpg';
 
-    // When A File Number Uploaded Files Gets More Than 3 It Shows A Toaster Massage.
-    upload_input.addEventListener('change', () => {
         file.push(upload_input.value);
-        if (file.length > 3) {new_toaster('alert', 'خطا!', 'تعداد فایل ها از 3 تا بیشتر نمیتواند باشد.')}
-    })
 
-    // Clicking On Input
-    upload_input.click();
+        // Clicking On Input
+        upload_input.click();
+    } else {swal('خطا!', 'تعداد فایل ها از 3 تا بیشتر نمیتواند باشد.', 'alert')}
 }
 
 // Adding Event Listener To Upload Media Button That Listens To Click And Calls 'upload_media' Function.
